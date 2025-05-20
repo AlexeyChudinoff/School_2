6fad9607a91b932feee3e4698fbc2cd31410c2fd
Alex <201900@mail.ru>
2025-05-20 23:54:58 +0700

    School_sql_4_измСвойств

---
+++ src/main/resources/liquibase/scripts/work on faculties.sql
@@ -0,0 +1,3 @@
+- liquibase formatted sql
+- changeset alexeychudinov:faculties_1
+CREATE INDEX get_faculty_name_color_idx on faculty (name,color)
--- src/main/resources/liquibase/scrits/user.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
--- changeset alexchudinov:1
-CREATE TABLE user (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-  )
